629176c90db8715501999ac0f1038c5509dd9f18
ashok9109 <aaaaashokyadav123@gmail>
2025-08-30 20:46:41 +0530

    Add images for technologies and implement interactive boxes in portfolio

---
+++ script.js
@@ -0,0 +1,16 @@
+// Box Active or Deactive
+
+let boxs = document.querySelectorAll(".box");
+
+boxs.forEach((box)=>{
+  box.addEventListener('click',function(){
+  deactive();
+  box.classList.add("active")  
+  })  
+});
+
+function deactive(){
+    boxs.forEach((box)=>{
+        box.classList.remove("active");
+    })
+};
